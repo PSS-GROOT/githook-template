1dcb7c2a9c1d88027922ec629886530cf4dd1b2f
nicksonthc <tan.hoo.chuan@pentamaster.com.my>
2021-12-19 19:07:46 +0800

    feat(init): init project , add githook

---
+++ githook-template/BusinessLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace githook_template
+{
+    public class BusinessLogic
+    {
+        public BusinessLogic()
+        {
+            IsCICD();
+        }
+
+        public Boolean IsCICD() {
+            return true;
+        } 
+    }
+}
---
+++ githook-template/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githook_template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CICD!");
+        }
+    }
+}
