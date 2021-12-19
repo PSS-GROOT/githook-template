c1610230974d04d791797ba5c6375e9d6ef9a652
nicksonthc <tan.hoo.chuan@pentamaster.com.my>
2021-12-19 19:09:46 +0800

    feat(test): add ms test

---
+++ mstest-githook/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using githook_template;
+
+
+namespace mstest_githook
+{
+    [TestClass]
+    public class UnitTestCICD
+    {
+        BusinessLogic bl = new BusinessLogic();
+
+        [TestMethod]
+        public void TestCICD()
+        {
+            
+            Assert.IsTrue(bl.IsCICD());
+        }
+    }
+}
---
+++ test-githook-template/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using githook_template;
+
+namespace test_githook_template
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCICD()
+        {
+            BusinessLogic bl = new BusinessLogic();
+            Assert.IsTrue(bl.IsCICD());
+        }
+    }
+}
